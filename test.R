b2e97c6dbf5802e6a8931ae661af446f2f4cc4a5
laptrinhr <103423856+laptrinhr@users.noreply.github.com>
2022-04-12 08:43:45 +0700

    Update test.R

--- test.R
+++ test.R
@@ -6,23 +6,4 @@ print (helloString) # print hello
 # declare a variable helloString
 helloString <- "Hello" 
 print (helloString) # print hello
-
-```
-
-```
-function test() {
-  console.log("notice the blank line before this function?");
-}
-```
-
-```
-function test() {
-  console.log("notice the blank line before this function?");
-}
-```
-
-```
-function test() {
-  console.log("notice the blank line before this function?");
-}
 ```
